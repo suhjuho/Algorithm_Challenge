52a0aebef4f75c338493f7f5d9e78a1d8b6abb15
suhjuho <sspaka0422@gmail.com>
2023-11-17 11:11:34 +0900

    Feat solve 택배배달과 수거하기

---
+++ programmers/level_2/택배배달과수거하기.js
@@ -0,0 +1,56 @@
+function solution(cap, n, deliveries, pickups) {
+  let answer = 0;
+  let leftDeliveries = deliveries.reduce((acc, cur) => acc + cur);
+  let leftPickups = pickups.reduce((acc, cur) => acc + cur);
+  let load = cap;
+  let last = n - 1;
+
+  while (leftDeliveries + leftPickups > 0) {
+    for (let house = last; house >= 0; house--) {
+      if (!!deliveries[house] || !!pickups[house]) {
+        last = house;
+        answer += (last + 1) * 2
+        break;
+      }
+    }
+
+    load = cap;
+
+    if (leftDeliveries > 0) {
+      for (let house = last; house >= 0; house--) {
+        if (load >= deliveries[house]) {
+          load = load - deliveries[house];
+          deliveries[house] = 0;
+        } else if (0 < load && load < deliveries[house]) {
+          deliveries[house] = deliveries[house] - load;
+          load = 0;
+        }
+
+        if (load === 0) break;
+      }
+    }
+
+    leftDeliveries -= (cap - load);
+
+    load = cap;
+
+    if (leftPickups > 0) {
+      for (let house = last; house >= 0; house--) {
+        if (load >= pickups[house]) {
+          load = load - pickups[house];
+          pickups[house] = 0;
+        } else if (0 < load && load < pickups[house]) {
+          pickups[house] = pickups[house] - load;
+          load = 0;
+        }
+
+        if (load === 0) break;
+      }
+    }
+
+    leftPickups -= (cap - load);
+
+  }
+
+  return answer;
+}
